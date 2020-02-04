55e1c9a43a55e8e3554974521c16510b01a2c93e
JR-2E <59854625+JR-2E@users.noreply.github.com>
2020-02-03 18:47:20 -0600

    Update Activity 6.py

--- Assignment 4/Activity 6.py
+++ Assignment 4/Activity 6.py
@@ -1,13 +1,12 @@
 # This program determines the amount of gallons that need to be bought to paint a room in the shape of a quadrilateral parallelagram.
 
-
+#get needed info from user
 print("What is the width of the room?")
 roomWidth = float(input())
 print("What is the height of the room?")
 roomHeight = float(input())
 print("What is the length of the room?")
 roomLength = float(input())
-
 print("How many square feet will the gallon of paint cover?")
 areaPerGallon = int(input())
 print("How much does a gallon of paint cost?")
