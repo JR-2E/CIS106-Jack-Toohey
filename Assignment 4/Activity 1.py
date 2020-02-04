eab05111f1a4b878d65d2485919af6641956a94b
JR-2E <59854625+JR-2E@users.noreply.github.com>
2020-02-03 18:49:30 -0600

    Update and rename Assignment 4-2.py to Activity 1.py

--- Assignment 4/Activity 1.py
+++ Assignment 4/Activity 1.py
@@ -1,12 +1,12 @@
 # This program calculates weekly, monthly, and annual gross income from hourly wage, and average hours worked per week.
 
-#collect needed info from user
+#get needed info from user
 print("How many hours of work do you average per week?")
 avgHoursPerWeek = float(input())
 print("How much money (USD) do you make per hour?")
 hourlyWage = float(input())
 
-#calculate income per various amounts of time
+#calc income per various amounts of time
 weeklyIncome = avgHoursPerWeek * hourlyWage
 yearlyIncome = weeklyIncome * 52
 monthlyIncome = yearlyIncome / 12
