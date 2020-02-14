b6cee36fdb63b53e30ec1eb8fc1e7e15a5efde83
Dave Braunschweig <dave@chicagocoding.com>
2020-02-14 09:10:54 -0600

    Assignment 5 corrections

--- Assignment 5/Activity 5.py
+++ Assignment 5/Activity 5.py
@@ -1,15 +1,18 @@
 # This program takes length and width in feet,
 # and converts it to area in square yards
 
+
 def get_length():
     print("Enter room length.")
     length = float(input())
     return length
 
+
 def get_width():
     print("Enter room width.")
     width = float(input())
     return width
+
 
 def calculate_area(length, width):
     area = length * width / 9
--- Assignment 5/Activity 6.py
+++ Assignment 5/Activity 6.py
@@ -2,42 +2,51 @@
 # of a room, and the coverage/cost of a gallon of paint,
 # and converts it to gallons needed and cost of paint.
 
+
 def get_length():
     print("Enter room length.")
     length = float(input())
     return length
+
 
 def get_width():
     print("Enter room width.")
     width = float(input())
     return width
 
+
 def get_height():
     print("Enter room height.")
     height = float(input())
     return height
+
 
 def get_cost():
     print("Enter the cost of one gallon of paint.")
     cost = float(input())
     return cost
 
+
 def get_coverage():
     print("Enter how many feet one gallon of paint covers.")
     coverage = float(input())
     return coverage
 
+
 def calculate_area(length, height, width):
     area = 2 * length * height + 2 * width * height
     return area
+
 
 def calculate_gallons(area, coverage):
     gallons = area / coverage + 0.9999
     return gallons
 
+
 def calculate_total_cost(gallons, cost):
     total_cost = gallons * cost
     return total_cost
+
 
 def display_result(gallons, total_cost):
     print("You will need " + str(int(gallons)) +
@@ -54,5 +63,6 @@ def main():
     gallons = calculate_gallons(area, coverage)
     total_cost = calculate_total_cost(gallons, cost)
     display_result(gallons, total_cost)
+
 
 main()
