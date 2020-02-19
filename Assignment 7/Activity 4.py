8899333761b3cf4fdbcb4366e439ad25065a4489
JR-2E <59854625+JR-2E@users.noreply.github.com>
2020-02-19 10:18:36 -0600

    Update Activity 4.py

--- Assignment 7/Activity 4.py
+++ Assignment 7/Activity 4.py
@@ -1,10 +1,15 @@
+# This program calculates area of
+# circles, rectangles, squares, and triangles.
+
+
 def processCircle():
     print("What is the radius of the circle?")
     radius = float(input())
     circleArea = radius * radius * 3.14159
     print("The area of the circle is " + str(circleArea) + " square units.")
-    
+
     return circleArea
+
 
 def processRectangle():
     print("What is the base of the shape?")
@@ -13,8 +18,9 @@ def processRectangle():
     rectangleHeight = float(input())
     rectangleArea = rectangleBase * rectangleHeight
     print("The area is " + str(rectangleArea) + " square units.")
-    
+
     return rectangleArea
+
 
 def processTriangle():
     print("Enter triangle base dimension.")
@@ -22,17 +28,21 @@ def processTriangle():
     print("Enter triangle height dimension.")
     triangleHeight = float(input())
     triangleArea = triangleBase * triangleHeight / 2
-    print("The area of the triangle is " + str(triangleArea) + " square units.")
-    
+    print("The area of the triangle is " +
+          str(triangleArea) + " square units.")
+
     return triangleArea
 
 # Main
-print("Which shape do you need to calculate the area of? A circle, triangle, square, rectangle, or rhombus?")
+print("Which shape do you need to calculate the area of?
+      A circle, triangle, square, rectangle, or rhombus?")
 choice = input()
 if choice == "circle" or choice == "Circle":
     processCircle()
 else:
-    if choice == "rectangle" or choice == "Rectangle" or choice == "square" or choice == "Square" or choice == "rhombus" or choice == "Rhombus":
+    if choice == "rectangle" or choice == "Rectangle" or
+    choice == "square" or choice == "Square" or
+    choice == "rhombus" or choice == "Rhombus":
         processRectangle()
     else:
         if choice == "triangle" or choice == "Triangle":
