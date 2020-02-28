db272c53da291eb21c5f53136e531296ddf0487b
Dave Braunschweig <dave@chicagocoding.com>
2020-02-28 11:58:26 -0600

    Assignment 7 file names

--- Assignment 7/Activity 4.py
+++ Assignment 7/Activity 4.py
@@ -1,66 +1,49 @@
-# This program calculates the area of circles, and rectangles.
+def calcCircleArea(radius):
+    circleArea = radius * radius * 3.14159    
+    return circleArea
 
+def calcRectangleArea(rectangleBase, rectangleHeight):
+    rectangleArea = rectangleBase * rectangleHeight    
+    return rectangleArea
 
-def get_choice():
-    print("Enter rectangle to calculate the area of a rectangle," +
-          " or circle to calculate the area of a circle.")
-    choice = input()
+def displayCircleArea(circleArea):
+    print("The area of the circle is " + str(circleArea) + " square units.")
+
+def displayRectangleArea(rectangleArea):
+    print("The area of the rectangle is " + str(rectangleArea) + " square units.")
+
+def getChoice():
+    print("Enter rectangle to calculate the area of a rectangle, or circle to calculate the area of a circle.")
+    choice = input()    
     return choice
 
-
-# First comes the circle functions
-def get_radius():
-    print("Enter the radius of the circle.")
-    radius = float(input())
+def getRadius():
+    print("What is the radius of the circle?")
+    radius = float(input())    
     return radius
 
-
-def calc_circle_area(radius):
-    circle_area = radius * radius * 3.14159
-    return circle_area
-
-
-def display_circle_area(circle_area):
-    print("The area of the circle is " +
-          str(circle_area))
-
-
-# Now for rectangle functions
-def get_rectangle_base():
-    print("What is the base of the rectangle?")
-    rectangle_base = float(input())
-    return rectangle_base
-
-
-def get_rectangle_height():
+def getRectangleHeight():
     print("What is the height of the rectangle?")
-    rectangle_height = float(input())
-    return rectangle_height
+    rectangleHeight = float(input())
+    
+    return rectangleHeight
 
+def getRectangleBase():
+    print("What is the base of the rectangle?")
+    rectangleBase = float(input())    
+    return rectangleBase
 
-def calc_rectangle_area(rectangle_base, rectangle_height):
-    rectangle_area = rectangle_base * rectangle_height
-    return rectangle_area
-
-
-def display_rectangle_area(rectangle_area):
-    print("The area of the rectangle is " +
-          str(rectangle_area))
-
-
-def main():
-    choice = get_choice()
+# Main
+choice = getChoice()
+if choice == "rectangle" or choice == "Rectangle":
+    rectangleBase = getRectangleBase()
+    rectangleHeight = getRectangleHeight()
+    rectangleArea = calcRectangleArea(rectangleBase, rectangleHeight)
+    displayRectangleArea(rectangleArea)
+else:
     if choice == "circle" or choice == "Circle":
-        radius = get_radius()
-        circle_area = calc_circle_area(radius)
-        circle_area = display_circle_area(circle_area)
-    elif choice == "rectangle" or choice == "Rectangle":
-        rectangle_base = get_rectangle_base()
-        rectangle_height = get_rectangle_height()
-        rectangle_area = calc_rectangle_area(rectangle_base, rectangle_height)
-        display_rectangle_area(rectangle_area)
+        radius = getRadius()
+        circleArea = calcCircleArea(radius)
+        displayCircleArea(circleArea)
     else:
         print("You must only enter either circle or rectangle.")
-
-
-main()
