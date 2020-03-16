c1bbe7e971048705d3ac145b77ab3eb3796f8866
Dave Braunschweig <dave@chicagocoding.com>
2020-03-16 08:06:09 -0500

    Assignment 9 corrections

--- Assignment 9/Activity 2.py
+++ Assignment 9/Activity 2.py
@@ -2,26 +2,24 @@ def directions():
     print("Choose an integer between 0 and 100 in your head. This program will try to find it. Input 'correct' when the program gets it right, or if your number is higher.")
 
 def loop():
-    guessAmount = 1
+    guessAmount = 0
     correct = 0
     high = 100
     low = 0
     while correct == 0:
-        guess = (high + low) / 2
-        print("Guess number " + str(guessAmount) + "." + " Is " + str(int(guess)) + " too high, too low, or is it correct?")
+        guess = int((high + low) / 2)
         guessAmount = guessAmount + 1
+        print("Guess number " + str(guessAmount) + "." + " Is " + str(guess) + " too high, too low, or is it correct (h,l,c)?")
         answer = input()
-        if answer == "High" or answer == "high":
-            high = guess
+        if answer == "High" or answer == "high" or answer == "H" or answer == "h":
             high = guess + 1
         else:
-            if answer == "Low" or answer == "low":
-                low = guess
+            if answer == "Low" or answer == "low" or answer == "L" or answer == "l":
                 low = guess - 1
             else:
-                if answer == "Correct" or answer == "correct":
+                if answer == "Correct" or answer == "correct" or answer == "C" or answer == "c":
                     correct = 1
-                    print("I have found your number! It is " + str(guess) + "!")
+                    print(f"I have found your number in {guessAmount} guesses! It is {guess}!")
                 else:
                     print("Your input was invalid")
 
