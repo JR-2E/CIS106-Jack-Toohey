0dd0281dc06150dfdce94b1a6dbd5d49c5f2d9c6
JR-2E <59854625+JR-2E@users.noreply.github.com>
2020-04-20 18:13:00 -0700

    Add files via upload

---
+++ Assignment 14/Activity 4.py
@@ -0,0 +1,57 @@
+import os
+
+def get_info():
+    print("Enter your first name, last name, street address, city," +
+          "state/province/region, and postal code, separated by commas.")
+    info = input()
+    return info
+
+
+def get_split_info(info):
+    info = ' '.join(info.split())
+    split_info = info.split(", ")
+    return split_info
+
+
+def create_file(split_info, address):
+    file = open(address, "w")
+    file.write(split_info[0] + " " + split_info[1] + "\n")
+    file.write(split_info[2] + "\n")
+    file.write(split_info[3] + ", " + split_info[4] + " " + split_info[5])
+    file.write("\n" + "\n")
+    file.close()
+
+
+def append_file(address):    
+    info = get_info()
+    split_info = get_split_info(info)
+    file = open(address, "a")
+    file.write(split_info[0] + " " + split_info[1] + "\n")
+    file.write(split_info[2] + "\n")
+    file.write(split_info[3] + ", " + split_info[4] + " " + split_info[5])
+    file.write("\n" + "\n")
+    file.close()
+    
+    
+def read_file(address):
+    file = open(address, "r")
+    for line in file:
+        line = line.strip()
+        print(line)
+    file.close()
+    print("")
+    
+
+def main():
+    address = "~addresses.txt"
+    if os.path.isfile(address):
+        print("File already exists.")
+        info = get_info()
+        split_info = get_split_info(info)
+        create_file(split_info, address)
+        append_file(address)
+        append_file(address)
+        read_file(address)
+
+        
+main()
