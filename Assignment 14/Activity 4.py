e10fccb71cebc0e2d126fcce4c70e4d27cce0e47
Dave Braunschweig <dave@chicagocoding.com>
2020-04-21 10:03:21 -0500

    Assignment 14 corrections and sample file

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -1,40 +1,6 @@
 import os
 
 
-def get_info():
-    print("Enter your first name, last name, street address, city," +
-          "state/province/region, and postal code, separated by commas.")
-    info = input()
-    return info
-
-
-def get_split_info(info):
-    info = info.replace(" ", "")
-    info = ' '.join(info.split())
-    split_info = info.split(",")
-    return split_info
-
-
-def create_file(split_info, address):
-    file = open(address, "w")
-    file.write(split_info[0] + " " + split_info[1] + "\n")
-    file.write(split_info[2] + "\n")
-    file.write(split_info[3] + ", " + split_info[4] + " " + split_info[5])
-    file.write("\n" + "\n")
-    file.close()
-
-
-def append_file(address):
-    info = get_info()
-    split_info = get_split_info(info)
-    file = open(address, "a")
-    file.write(split_info[0] + " " + split_info[1] + "\n")
-    file.write(split_info[2] + "\n")
-    file.write(split_info[3] + ", " + split_info[4] + " " + split_info[5])
-    file.write("\n" + "\n")
-    file.close()
-
-
 def read_file(address):
     file = open(address, "r")
     for line in file:
@@ -45,14 +11,9 @@ def read_file(address):
 
 
 def main():
-    address = "~addresses.txt"
+    address = "address.txt"
     if os.path.isfile(address):
         print("File already exists.")
-        info = get_info()
-        split_info = get_split_info(info)
-        create_file(split_info, address)
-        append_file(address)
-        append_file(address)
         read_file(address)
 
 
