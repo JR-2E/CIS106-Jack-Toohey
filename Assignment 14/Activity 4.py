86bdc0cdbf6631ee5c86eb28263332525b1d2b7f
JR-2E <59854625+JR-2E@users.noreply.github.com>
2020-04-21 09:12:51 -0700

    Update Activity 4.py

--- Assignment 14/Activity 4.py
+++ Assignment 14/Activity 4.py
@@ -1,4 +1,5 @@
-import os
+# This program reads from a address file,
+# and outputs the addresses in a certain format.
 
 
 def read_file(address):
@@ -10,11 +11,41 @@ def read_file(address):
     print("")
 
 
+def get_file_len(addresses):
+    with open(addresses) as f:
+        for file_len, l in enumerate(f):
+            pass
+    file_len = file_len + 1
+    return file_len
+
+
+def check_len(addresses, file_len):
+    if file_len % 4:
+        with open('~addresses.txt', 'r') as f:
+            flat_list = [word for line in f for word in line.split()]
+            for x in range(0, (file_len), 8):
+                print(flat_list[x + 1] + ", " + flat_list[x] + ", " +
+                      flat_list[x + 2] + " " + flat_list[x + 3] + " " +
+                      flat_list[x + 4] + ", " + flat_list[x + 5] + " " +
+                      flat_list[x + 6] + ", " + flat_list[x + 7])
+            print(flat_list[file_len * 2 - 5] + ", " +
+                  flat_list[file_len * 2 - 6] + ", " +
+                  flat_list[file_len * 2 - 4] + " " +
+                  flat_list[file_len * 2 - 3] + " " +
+                  flat_list[file_len * 2 - 2] + ", " +
+                  flat_list[file_len * 2 - 1] + flat_list[file_len * 2 + 1])
+    else:
+        print("File line count is incorrect for format. " +
+              "Make sure there is no more than one" +
+              "blank line after the final address.")
+
+
 def main():
-    address = "address.txt"
-    if os.path.isfile(address):
-        print("File already exists.")
-        read_file(address)
+    addresses = "~addresses.txt"
+    file_len = get_file_len(addresses)
+    check_len(addresses, file_len)
 
-
+f = open('~addresses.txt')
+lines = f.readlines()
 main()
+
